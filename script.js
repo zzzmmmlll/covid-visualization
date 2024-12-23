8daa388b2e9d55fc1f505575051af2e788c6a25a
“zzzmmmlll” <286293834@qq.com>
2024-12-23 21:33:15 +0800

    Update index.html and script.js

--- script.js
+++ script.js
@@ -39,25 +39,25 @@ d3.csv("https://raw.githubusercontent.com/zzzmmmlll/covid-visualization/refs/hea
             const width = 900, height = 500;
             const margin = { top: 20, right: 30, bottom: 50, left: 60 };
 
-            // 创建SVG画布
+            // 创建SVG画布，并将其居中
             const svg = d3.select("#chart").html("").append("svg")
-                .attr("width", width)
-                .attr("height", height)
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
                 .append("g")
                 .attr("transform", `translate(${margin.left},${margin.top})`);
 
             // 设置X轴和Y轴的比例尺
             const x = d3.scaleTime()
                 .domain(d3.extent(countryData, d => d.Date))
-                .range([0, width - margin.left - margin.right]);
+                .range([0, width]);
 
             const y = d3.scaleLinear()
                 .domain([0, d3.max(countryData, d => Math.max(d.Confirmed, d.Deaths, d.Recovered))])
-                .range([height - margin.top - margin.bottom, 0]);
+                .range([height, 0]);
 
             // 绘制X轴
             svg.append("g")
-                .attr("transform", `translate(0,${height - margin.top - margin.bottom})`)
+                .attr("transform", `translate(0,${height})`)
                 .call(d3.axisBottom(x).ticks(d3.timeMonth));
 
             // 绘制Y轴
@@ -77,29 +77,16 @@ d3.csv("https://raw.githubusercontent.com/zzzmmmlll/covid-visualization/refs/hea
                 .x(d => x(d.Date))
                 .y(d => y(d.Recovered));
 
-            svg.append("path")
-                .data([countryData])
+            // 添加折线
+            const lines = svg.selectAll(".line")
+                .data([lineConfirmed, lineDeaths, lineRecovered])
+                .enter().append("path")
                 .attr("class", "line")
                 .attr("fill", "none")
-                .attr("stroke", "blue")
+                .attr("stroke", (d, i) => i === 0 ? "blue" : i === 1 ? "red" : "green")
                 .attr("stroke-width", 2)
-                .attr("d", lineConfirmed);
-
-            svg.append("path")
-                .data([countryData])
-                .attr("class", "line")
-                .attr("fill", "none")
-                .attr("stroke", "red")
-                .attr("stroke-width", 2)
-                .attr("d", lineDeaths);
-
-            svg.append("path")
-                .data([countryData])
-                .attr("class", "line")
-                .attr("fill", "none")
-                .attr("stroke", "green")
-                .attr("stroke-width", 2)
-                .attr("d", lineRecovered);
+                .attr("d", (d, i) => i === 0 ? lineConfirmed(countryData) :
+                    i === 1 ? lineDeaths(countryData) : lineRecovered(countryData));
 
             // 添加图例
             const legend = svg.append("g")
@@ -151,14 +138,9 @@ d3.csv("https://raw.githubusercontent.com/zzzmmmlll/covid-visualization/refs/hea
                 const newY = transform.rescaleY(y);
 
                 svg.selectAll(".line")
-                    .attr("d", function (d) {
-                        if (d === countryData) {
-                            return lineConfirmed(d);
-                        } else if (d === countryData) {
-                            return lineDeaths(d);
-                        } else {
-                            return lineRecovered(d);
-                        }
+                    .attr("d", function (d, i) {
+                        return i === 0 ? lineConfirmed(countryData) :
+                            i === 1 ? lineDeaths(countryData) : lineRecovered(countryData);
                     });
 
                 svg.selectAll("g.x-axis")
@@ -167,36 +149,42 @@ d3.csv("https://raw.githubusercontent.com/zzzmmmlll/covid-visualization/refs/hea
                 svg.selectAll("g.y-axis")
                     .call(d3.axisLeft(newY));
             }
+
+            // 创建Tooltip
+            const tooltip = d3.select("body").append("div")
+                .attr("class", "tooltip")
+                .style("position", "absolute")
+                .style("visibility", "hidden")
+                .style("background", "rgba(0,0,0,0.6)")
+                .style("color", "#fff")
+                .style("border-radius", "5px")
+                .style("padding", "10px")
+                .style("font-size", "12px")
+                .style("pointer-events", "none");
+
+            // 给每条折线添加鼠标悬停事件
+            svg.selectAll(".line")
+                .on("mouseover", function (event, d) {
+                    tooltip.style("visibility", "visible");
+                })
+                .on("mousemove", function (event, d) {
+                    tooltip.style("top", (event.pageY + 10) + "px")
+                        .style("left", (event.pageX + 10) + "px")
+                        .text(function (d) {
+                            if (d === lineConfirmed) {
+                                return `Confirmed: ${d.Confirmed}`;
+                            } else if (d === lineDeaths) {
+                                return `Deaths: ${d.Deaths}`;
+                            } else {
+                                return `Recovered: ${d.Recovered}`;
+                            }
+                        });
+                })
+                .on("mouseout", function () {
+                    tooltip.style("visibility", "hidden");
+                });
         }
-        // 创建Tooltip
-        const tooltip = d3.select("body").append("div")
-            .attr("class", "tooltip")
-            .style("position", "absolute")
-            .style("visibility", "hidden")
-            .style("background", "rgba(0,0,0,0.6)")
-            .style("color", "#fff")
-            .style("border-radius", "5px")
-            .style("padding", "10px")
-            .style("font-size", "12px")
-            .style("pointer-events", "none");
-        svg.append("path")
-            .data([countryData])
-            .attr("class", "line")
-            .attr("fill", "none")
-            .attr("stroke", "blue")
-            .attr("stroke-width", 2)
-            .attr("d", lineConfirmed)
-            .on("mouseover", function (event, d) {
-                tooltip.style("visibility", "visible")
-                    .text(`Confirmed: ${d.Confirmed}`);
-            })
-            .on("mousemove", function (event) {
-                tooltip.style("top", (event.pageY + 10) + "px")
-                    .style("left", (event.pageX + 10) + "px");
-            })
-            .on("mouseout", function () {
-                tooltip.style("visibility", "hidden");
-            });
+
         // 创建选择框
         const filterContainer = d3.select("#filter-container")
             .append("input")
@@ -222,8 +210,6 @@ d3.csv("https://raw.githubusercontent.com/zzzmmmlll/covid-visualization/refs/hea
                     .remove();
             }
         }
-
-
     })
     .catch(function (error) {
         console.log(error);
